6bd9015aaff0aef136eef9247f03e1a0db662aab
GokselKUCUKSAHIN <gokselkucuksahin@gmail.com>
2022-03-09 09:29:15 +0300

    `Array<T> cot` Implemented and Tested.

---
+++ yuki/func/array/array-cot.ts
@@ -0,0 +1,24 @@
+import {safePrototypePatch} from "../../define-prototype";
+import {fastMap} from "./fast-map";
+import {cot} from "../math/cot";
+
+fastMap.monkeyPatch();
+cot.monkeyPatch();
+
+declare global {
+    interface Array<T> {
+        cot(): number[];
+    }
+}
+
+arrayCot.monkeyPatch = (): void => {
+    safePrototypePatch(Array, 'cot',
+        function () {
+            return arrayCot(this);
+        }
+    );
+}
+
+export function arrayCot(thisArray: number[]): number[] {
+    return thisArray.fastMap(Math.cot);
+}
