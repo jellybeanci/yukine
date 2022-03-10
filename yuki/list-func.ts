12aea10eadc3168e3bdcf29e519880591c2d8568
GokselKUCUKSAHIN <gokselkucuksahin@gmail.com>
2022-03-10 12:14:18 +0300

    printTree Implemented.

--- yuki/list-func.ts
+++ yuki/list-func.ts
@@ -10,8 +10,7 @@ async function deepDirList(path: PathLike): Promise<Dirent[]> {
 
 async function printDirTable(path: string): Promise<void> {
     const methods = ['isBlockDevice', 'isCharacterDevice', 'isDirectory', 'isFIFO', 'isFile', 'isSocket', 'isSymbolicLink'];
-    const libPath = join(__dirname, path);
-    const results = await deepDirList(libPath);
+    const results = await deepDirList(join(__dirname, path));
     const table = results.map(result => {
         const cur = {name: result.name};
         for (const method of methods) {
@@ -22,13 +21,17 @@ async function printDirTable(path: string): Promise<void> {
     console.table(table);
 }
 
-async function dumpDir(entyPoint: string): Promise<any> {
-    const currentDir = await readdir(entyPoint, {withFileTypes: true, encoding: "utf-8"});
+async function dumpDir(entryPoint: string): Promise<any> {
+    const currentDir = await readdir(entryPoint, {withFileTypes: true, encoding: "utf-8"});
     return Promise.all(currentDir.map(
         async dirent => dirent.isDirectory()
-            ? await dumpDir(join(entyPoint, dirent.name))
+            ? await dumpDir(join(entryPoint, dirent.name))
             : dirent.name)
     );
+}
+
+function tabber(tabCount: number = 1) {
+    return "  ".repeat(tabCount);
 }
 
 class Node extends Object {
@@ -45,17 +48,25 @@ class Node extends Object {
         this.childeren = childeren;
     }
 
-    override toString(): string {
-        return `${this.head}:\r\t${this.childeren.length > 0 ? this.childeren : ""}`;
+    printTree(tabCount: number): string {
+        const header = `${this.head}`;
+        let sub = "";
+        if (this.childeren) {
+            sub += `\n${tabber(tabCount)}║\n${tabber(tabCount)}╚ ${this.childeren.map(child => child.printTree(tabCount + 1)).join("\n" + tabber(tabCount + 1))}`
+        }
+        return header + sub;
     }
 
+    override toString(): string {
+        return this.printTree(1)
+    }
 }
 
-async function dumpDirObject(entyPoint: string): Promise<Node[]> {
-    const currentDir = await readdir(entyPoint, {withFileTypes: true, encoding: "utf-8"});
+async function dumpDirObject(entryPoint: string): Promise<Node[]> {
+    const currentDir = await readdir(entryPoint, {withFileTypes: true, encoding: "utf-8"});
     return Promise.all(currentDir.map(async dirent => {
             const node = new Node(dirent.name);
-            if (dirent.isDirectory()) node.setChilderen(await dumpDirObject(join(entyPoint, dirent.name)));
+            if (dirent.isDirectory()) node.setChilderen(await dumpDirObject(join(entryPoint, dirent.name)));
             return node;
         })
     );
@@ -64,5 +75,10 @@ async function dumpDirObject(entyPoint: string): Promise<Node[]> {
 (async main => {
     const entryPoint = join(__dirname, "func");
     const dirArray = await dumpDirObject(entryPoint);
-    console.dir(dirArray, {'depth': null})
+    const root = new Node("func", dirArray);
+    // console.dir(dirArray, {'depth': null});
+    // console.log(JSON.stringify(dirArray));
+    console.log(root.toString())
+
+
 })();
