541a28ee91c67f40a79b216e3c5515bd15135ade
Jellybeanci <gokselkucuksahin@gmail.com>
2022-03-10 04:56:34 +0300

    list-func object dumper.

--- yuki/list-func.ts
+++ yuki/list-func.ts
@@ -31,9 +31,38 @@ async function dumpDir(entyPoint: string): Promise<any> {
     );
 }
 
+class Node extends Object {
+    private head: string;
+    private childeren: Node[] = undefined;
+
+    constructor(head: string, child?: Node[]) {
+        super();
+        this.head = head;
+        this.childeren = child;
+    }
+
+    setChilderen(childeren: Node[]): void {
+        this.childeren = childeren;
+    }
+
+    override toString(): string {
+        return `${this.head}:\r\t${this.childeren.length > 0 ? this.childeren : ""}`;
+    }
+
+}
+
+async function dumpDirObject(entyPoint: string): Promise<Node[]> {
+    const currentDir = await readdir(entyPoint, {withFileTypes: true, encoding: "utf-8"});
+    return Promise.all(currentDir.map(async dirent => {
+            const node = new Node(dirent.name);
+            if (dirent.isDirectory()) node.setChilderen(await dumpDirObject(join(entyPoint, dirent.name)));
+            return node;
+        })
+    );
+}
+
 (async main => {
     const entryPoint = join(__dirname, "func");
-    const dirArray = await dumpDir(entryPoint);
-    console.log(dirArray)
-    // await printDirTable("func")
+    const dirArray = await dumpDirObject(entryPoint);
+    console.dir(dirArray, {'depth': null})
 })();
